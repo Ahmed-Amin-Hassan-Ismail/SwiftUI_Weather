81f74ef1d3bad8746f1b417130c76eee830045d9
Ahmed Amin Hassan <AhmedAminHassanIsmail@gmail.com>
2024-01-16 22:14:01 +0200

    initial commit

---
+++ Weather/Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Ahmed Amin on 16/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
